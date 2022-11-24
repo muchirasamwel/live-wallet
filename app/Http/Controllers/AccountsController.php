53ccf24e681e5c8d24e62c685f166f60076308d0
muchirasamwel <muchirasamwel@gmail.com>
2022-11-24 11:31:22 +0300

    * add accounts fetching api * Connect project to the DB

---
+++ app/Http/Controllers/AccountsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Accounts;
+use Illuminate\Http\Request;
+
+class AccountsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $allAccounts = Accounts::all();
+        return response()->json($allAccounts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Accounts  $accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Accounts $accounts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Accounts  $accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Accounts $accounts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Accounts  $accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Accounts $accounts)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('accounts', \App\Http\Controllers\AccountsController::class);
