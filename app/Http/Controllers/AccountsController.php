3171f934109f8c1f590ad46ce8c98386fe4c9026
muchirasamwel <muchirasamwel@gmail.com>
2022-11-28 14:38:45 +0300

    * Add redis server * Use redis server to cache all accounts api response

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -4,24 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Accounts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
 
 class AccountsController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        $allAccounts = Accounts::all();
+        $allAccounts = json_decode(Redis::get('allAccounts'));
+        if ($allAccounts)
+            Log::info('redis data available..');
+
+        if (!$allAccounts) {
+            $allAccounts = Accounts::where('balance', '>', 0)->where('balance','<',500)->where('name', '!=', 'name 2')->get();
+            Log::info('Saving new redis data..');
+            Redis::set('allAccounts', $allAccounts, 'EX', (1 * 60));
+        }
+
         return response()->json($allAccounts);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -32,7 +43,7 @@ class AccountsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Accounts  $accounts
+     * @param \App\Models\Accounts $accounts
      * @return \Illuminate\Http\Response
      */
     public function show(Accounts $accounts)
@@ -43,8 +54,8 @@ class AccountsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Accounts  $accounts
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Accounts $accounts
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Accounts $accounts)
@@ -55,7 +66,7 @@ class AccountsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Accounts  $accounts
+     * @param \App\Models\Accounts $accounts
      * @return \Illuminate\Http\Response
      */
     public function destroy(Accounts $accounts)
